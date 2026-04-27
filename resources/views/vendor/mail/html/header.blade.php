0542c5f0abb1384d6ed30ce3ee781042de3b0ecc
Tahani Alhammad <tahani_alhammad@yahoo.com>
2026-04-27 19:24:43 +0200

    update

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ config('app.url') }}/images/logo.png" class="logo" alt="{{ config('app.name') }} Logo">
+</a>
+</td>
+</tr>
